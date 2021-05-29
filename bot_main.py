391dcf6d69690b8c4a26f197b8ca11e816165931
aleksei.alekseev <aleksei.alekseev@corp.mail.ru>
2021-05-29 17:00:28 +0300

    Bot template for testing

---
+++ bot_main.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('1874578805:AAFwPETVFNYBBBkXPZxpWJkPyROAByw50XQ')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+bot.polling(none_stop=True)
